01beac5c6db3dbf69cf69626cc763102375269e8
Chaiwat <boyChaiwat1997@gmail.com>
2022-09-18 15:18:11 +0700

    Update React App : add service refresh token

--- react-app/src/service/auth.service.js
+++ react-app/src/service/auth.service.js
@@ -24,10 +24,27 @@ export const GetToken =  (code) => {
 
 export const RefaceToken = () => {
 
+    let baseUrl = import.meta.env.VITE_AUTH_URL;
+    const params = new URLSearchParams();
+    params.append('refresh_token', localStorage.getItem('refresh_token'));
+    params.append('grant_type', 'refresh_token');
+
+    return axios.post(baseUrl + '/oauth2/token',params, { 
+        auth: {
+            username: import.meta.env.VITE_AUTH_CLIENT_ID,
+            password: import.meta.env.VITE_AUTH_CLIENT_SECRET
+        },
+        headers:{'Content-Type': 'application/x-www-form-urlencoded'}
+    });
 }
 
 export const SetToken = (token) => {
     localStorage.setItem('access_token',token.access_token);
     localStorage.setItem('refresh_token',token.refresh_token);
+}
+
+export const ClearToken = () => {
+    localStorage.removeItem('access_token');
+    localStorage.removeItem('refresh_token');
 }
 
